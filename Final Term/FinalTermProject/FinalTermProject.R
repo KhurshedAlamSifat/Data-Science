af353c3f0a3ec86151d090597c8efb43a22c4e6e
Khurshed Alam Sifat <89679343+KhurshedAlamSifat@users.noreply.github.com>
2023-08-11 00:57:24 +0600

    normalize attribute

--- Final Term/FinalTermProject/FinalTermProject.R
+++ Final Term/FinalTermProject/FinalTermProject.R
@@ -4,3 +4,15 @@ sum(is.na(heart_disease))
 sum(heart_disease=="")
 str(heart_disease)
 summary(heart_disease)
+
+attributes_to_normalize <- setdiff(names(heart_disease), "target")
+
+min_max_normalize <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+for (attr in attributes_to_normalize) {
+  heart_disease[[attr]] <- min_max_normalize(heart_disease[[attr]])
+}
+
+head(heart_disease)
