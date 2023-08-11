dc3a56526b6a9761da71801506fbb2e84c6dfc93
Khurshed Alam Sifat <89679343+KhurshedAlamSifat@users.noreply.github.com>
2023-08-12 00:22:28 +0600

    apply knn

--- Final Term/FinalTermProject/FinalTermProject.R
+++ Final Term/FinalTermProject/FinalTermProject.R
@@ -18,6 +18,7 @@ for (attr in attributes_to_normalize) {
 head(heart_disease)
 
 correlation_matrix <- cor(heart_disease)
+heatmap(correlation_matrix)
 
 mask_lower_triangle <- function(mat) {
   mat[lower.tri(mat, diag = TRUE)] <- ""
@@ -26,5 +27,48 @@ mask_lower_triangle <- function(mat) {
 
 upper_triangle <- mask_lower_triangle(correlation_matrix)
 
-sum(upper_triangle>0.4 & upper_triangle<1)
-upper_triangle>1 & upper_triangle<0.8
+sum(upper_triangle>0.05 & upper_triangle<0)
+
+library("caret")
+library("class")
+
+set.seed(123)
+train_index <- createDataPartition(heart_disease$target, p = 0.7, list = FALSE)
+train_data <- heart_disease[train_index, ]
+test_data <- heart_disease[-train_index, ]
+
+train_features <- train_data[, -which(names(train_data) == "target")]
+train_target <- train_data$target
+test_features <- test_data[, -which(names(test_data) == "target")]
+test_target <- test_data$target
+
+knn_model <- knn(train_features, test_features, train_target, k = 5)
+
+accuracy <- mean(knn_model == test_target)
+cat("Accuracy (Dividing Data into Training and Test Set):", accuracy, "\n")
+
+
+heart_disease$target <- as.factor(heart_disease$target)
+
+control <- trainControl(method = "cv", number = 10)
+
+knn_model_cv <- train(target ~ ., data = heart_disease, method = "knn",
+                      trControl = control, preProcess = c("center", "scale"))
+
+accuracy_cv <- knn_model_cv$results$Accuracy
+cat("Accuracy (10-Fold Cross-Validation):", mean(accuracy_cv), "\n")
+
+knn_predictions <- predict(knn_model_cv, newdata = heart_disease)
+conf_matrix <- confusionMatrix(knn_predictions, heart_disease$target)
+
+true_positive <- conf_matrix$table[2, 2]
+false_positive <- conf_matrix$table[1, 2]
+true_negative <- conf_matrix$table[1, 1]
+false_negative <- conf_matrix$table[2, 1]
+
+recall <- true_positive / (true_positive + false_negative)
+precision <- true_positive / (true_positive + false_positive)
+
+print(conf_matrix)
+cat("Recall:", recall, "\n")
+cat("Precision:", precision, "\n")
