4052250b6cab0f38c5d7dd7fb533642f8680f885
Khurshed Alam Sifat <89679343+KhurshedAlamSifat@users.noreply.github.com>
2023-08-15 15:18:29 +0600

    update in normalixation

--- Final Term/FinalTermProject/FinalTermProject.R
+++ Final Term/FinalTermProject/FinalTermProject.R
@@ -1,5 +1,5 @@
 heart_disease <- read.csv("C:/Users/Khurshed Alam Sifat/Documents/Data Science/Final Term/heart.csv", header = TRUE, sep = ",")
-view(heart_disease)
+View(heart_disease)
 
 sum(is.na(heart_disease))
 sum(heart_disease=="")
@@ -8,7 +8,9 @@ summary(heart_disease)
 
 attributes_to_normalize <- setdiff(names(heart_disease), "target")
 min_max_normalize <- function(x){(x - min(x)) / (max(x) - min(x))}
-for (attr in attributes_to_normalize){heart_disease[[attr]] <- min_max_normalize(heart_disease[[attr]])}
+for (each_attribute in attributes_to_normalize){
+  heart_disease[[each_attribute]] <- min_max_normalize(heart_disease[[each_attribute]])
+  }
 head(heart_disease)
 
 correlation_matrix <- cor(heart_disease)
